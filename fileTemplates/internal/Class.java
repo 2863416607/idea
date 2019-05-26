5072b0eb656cc34d321703c63d331726d3c42cf8
CYM <2863416607@qq.com>
2019-05-26 19:20:56 +0800

    IU-2019.1.2 <CYM@DESKTOP-935RCKO Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, _@user_Default.icls, project.default.xml, rainbow_brackets.xml, Enum.java, Default copy.xml, Interface.java, lombok-plugin.xml, Default.xml, file.template.settings.xml, ignore.xml, code.style.schemes, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, keymap.xml, javaeeExternalResources.xml, androidEditors.xml, colors.scheme.xml, _@user_Material Oceanic.icls, _@user_Darcula.icls, Freemarker.ftl, databaseDrivers.xml, mavenVersion.xml, templates.xml, javarebel.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, gitee.xml, path.macros.xml, mavenRunHelper.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, mybatis.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * @Auther: 陈远明
+ * @Date: $DATE $HOUR : $MINUTE
+ * @Description: 
+ */
+public class ${NAME} {
+}
---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @Auther: 陈远明
+ * @Date: $DATE $HOUR : $MINUTE
+ * @Description: 
+ */
+public interface ${NAME} {
+}
